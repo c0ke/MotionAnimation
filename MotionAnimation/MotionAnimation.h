81efc6e1908a4750ef0cdbf2bb9e61b5ddec80be
Jakkapong Yuangtong <visualizeq@gmail.com>
2017-01-31 13:58:57 +0700

    support carthage

---
+++ MotionAnimation/MotionAnimation.h
@@ -0,0 +1,19 @@
+//
+//  MotionAnimation.h
+//  MotionAnimation
+//
+//  Created by Coke on 1/31/17.
+//  Copyright © 2017 Coke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MotionAnimation.
+FOUNDATION_EXPORT double MotionAnimationVersionNumber;
+
+//! Project version string for MotionAnimation.
+FOUNDATION_EXPORT const unsigned char MotionAnimationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MotionAnimation/PublicHeader.h>
+
+
